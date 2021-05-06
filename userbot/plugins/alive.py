1ecb7bbdf5ba3b93909d3ae2ab554dda5d15205f
ᗪᖇᗩᒍ <72666713+draj48@users.noreply.github.com>
2021-05-06 19:10:24 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -3,7 +3,7 @@ from platform import python_version
 
 from telethon import version
 
-from . import ALIVE_NAME, StartTime, catversion, get_readable_time, mention, reply_id
+from . import ALIVE_NAME, StartTime, nexgenv, get_readable_time, mention, reply_id
 
 DEFAULTUSER = ALIVE_NAME or "NᴇxGEN"
 CAT_IMG = Config.ALIVE_PIC
